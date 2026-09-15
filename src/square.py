9b1a3f0a022daf54f642d9c398cacac7c4f5a66e
CoolNikki <mycoolworkmail@gmail.com>
2026-09-15 14:51:10 -0600

    2->4

--- src/square.py
+++ src/square.py
@@ -2,11 +2,11 @@ class Square:
 
     # FIXME
     def __init__(self, side) -> None:
-        pass
-
-    def area(self): 
-        return self.side * self.side 
+        if side <= 0:
+            self.side = 1
+        else:
+            self.side = side
     
     # FIXME
     def perimeter(self): 
-        return self.side * 2
+        return self.side * 4
